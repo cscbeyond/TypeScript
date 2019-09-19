ae83fd2f679639e77ac01e5fc69273ffed93f93f
geekchen <cscbeyond@sina.com>
2019-09-19 23:26:26 +0800

    把类作为参数类型的泛型类

--- 15-把类作为参数类型的泛型类/index.ts
+++ 15-把类作为参数类型的泛型类/index.ts
@@ -1,16 +1,4 @@
-/*
-    定义一个User类，这个类的作用就是映射数据库字段
-    然后定义一个MysqDb的类，这个类用于操作数据库
-    然后把User类作为参数传入到MysqlDb中
-*/
-// var user = new User({
-//     username: '张三',
-//     password: '123456'
-// })
-
-// var Db = new MysqlDb();
-// Db.add(user);
-
+// 把类作为参数来约束数据传入的类型
 /*
 class User {
     username: string | undefined;
@@ -19,6 +7,8 @@ class User {
 
 class MysqlDb {
     add(user: User): boolean {
+
+        console.log(user);
         return true;
     }
 }
@@ -27,9 +17,10 @@ var u = new User();
 u.username = '张三';
 u.password = '123456';
 
-var DB = new MysqlDb();
-DB.add(u);
+var Db = new MysqlDb();
+Db.add(u);
 */
+
 /*
 class ArticleCate {
     title: string | undefined;
@@ -38,47 +29,52 @@ class ArticleCate {
 }
 
 class MysqlDb {
-    add(info:ArticleCate):boolean {
-        console.log(info);
+    add(info: ArticleCate): boolean {
+        console.log(info.title);
         return true;
     }
 }
+
 var a = new ArticleCate();
 a.title = '国内';
 a.desc = '国内新闻';
 a.status = 1;
 
 var Db = new MysqlDb();
-Db.add(a); 
+Db.add(a);
 */
 
+
+// 以上两段代码：每使用一次MySqlDb，就得重新写一遍，导致代码重复
+
+// 使用泛型 可以避免代码重复问题
+/*
 // 操作数据库的泛型类
-class MysqlDb<T> {
+class MysqlDb<T>{
     add(info: T): boolean {
         console.log(info);
         return true;
     }
-    update(info: T, id: number): boolean {
-        console.log(info);
-        console.log(id);
-        return true;
-    }
 }
 
-
 // 想给User表增加数据
-//1.定义一个User类和数据库进行映射
-// class User{
-//     username: string | undefined;
-//     password:string | undefined;
-// }
-// var u = new User();
-// u.username = '张三';
-// u.password = '123456';
-// var Db = new MysqlDb();
-// Db.add(u);
+// 1.定义一个User类，和数据库进行映射
 
-//2.定义一个文章分类的类 和数据库进行映射
+class User {
+    username: string | undefined;
+    password: string | undefined;
+}
+
+var u = new User();
+u.username = '张三';
+u.password = '123456';
+
+var Db = new MysqlDb();
+
+Db.add(u);
+
+*/
+// 2.定义一个ArticleCate类，和数据库进行映射
 
 class ArticleCate {
     title: string | undefined;
@@ -87,28 +83,38 @@ class ArticleCate {
     constructor(params: {
         title: string | undefined,
         desc: string | undefined,
-        status?: number | undefined
+        status?: number | undefined,
     }) {
         this.title = params.title;
         this.desc = params.desc;
         this.status = params.status;
     }
 }
+class MysqlDb<T>{
+    add(info: T): boolean {
+        console.log(info);
+        return true;
+    }
+    update(info: T, id: number): boolean {
+        console.log(info);
+        console.log(id);
 
+        return true;
+    }
+}
+// 增加
+// var a = new ArticleCate({
+//     title: '分类',
+//     desc: '123'
+// });
+// var Db = new MysqlDb<ArticleCate>();
+// Db.add(a);
+
+//更新
 var a = new ArticleCate({
-    title: '分类',
-    desc: '1111'
+    title: '分类111',
+    desc: '456'
 });
-//类当做参数的泛型类
-var Db = new MysqlDb<ArticleCate>();
-Db.add(a);
 a.status = 0;
+var Db = new MysqlDb<ArticleCate>();
 Db.update(a, 12);
-
-
-
-
-
-
-
-
